eb933aa7fcd87da83f451d011a5909ad63bb20eb
Clément Voisin <clembs@clembs.com>
2024-10-06 18:51:54 +0200

    feat: text input error text, prefix icons

--- src/lib/components/TextInput.svelte
+++ src/lib/components/TextInput.svelte
@@ -1,42 +1,92 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte';
 	import type { HTMLInputAttributes } from 'svelte/elements';
 
 	let {
 		type,
 		label,
+		error,
 		name,
 		required = true,
+		prefixIcon,
 		value = $bindable(''),
 		...restProps
 	}: HTMLInputAttributes & {
+		name: string;
 		label: string;
+		error?: string;
+		prefixIcon?: Snippet<[number]>;
 	} = $props();
 </script>
 
-<div class="text-input">
-	<label for={name}>
+<label class="text-input" for={name}>
+	<div class="label-text">
 		{label}
-	</label>
-	<input {type} id={name} {name} {...restProps} bind:value />
-</div>
+	</div>
+
+	<div class="input" class:error>
+		{#if prefixIcon}
+			<div class="prefix-icon">
+				{@render prefixIcon(24)}
+			</div>
+		{/if}
+		<input {type} id={name} {name} {...restProps} bind:value />
+	</div>
+
+	{#if error}
+		<p class="error">{error}</p>
+	{/if}
+</label>
 
 <style lang="scss">
 	.text-input {
 		display: flex;
 		flex-direction: column;
-		gap: 0.5rem;
 		width: 100%;
 
-		label {
+		.label-text,
+		.error {
 			font-weight: 600;
 			font-size: 0.815rem;
 		}
 
-		input {
-			padding: 0.75rem 1rem;
+		.input {
+			display: flex;
+			align-items: center;
 			border: 1px solid #000;
 			border-radius: 0.5rem;
-			background-color: #ededed;
+			margin-top: 0.5rem;
+
+			&.error {
+				border-color: #e90000;
+				outline: 1px solid #e90000;
+			}
+
+			.prefix-icon {
+				display: grid;
+				place-items: center;
+				padding: 0.75rem;
+				border-radius: 0.5rem 0 0 0.5rem;
+				border-right: 1px solid #000;
+				background-color: #d9d9d9;
+				height: 3rem;
+			}
+
+			input {
+				padding: 0.75rem 1rem;
+				border-radius: 0.5rem;
+				width: 100%;
+				border: transparent;
+				background-color: #ededed;
+			}
+
+			.prefix-icon + input {
+				border-radius: 0 0.5rem 0.5rem 0;
+			}
+		}
+
+		.error {
+			color: #e90000;
 		}
 	}
 </style>
