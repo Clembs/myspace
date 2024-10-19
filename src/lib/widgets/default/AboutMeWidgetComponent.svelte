b583cd23e8bbb101d9f2066c365769b6d69db1aa
Clément Voisin <clembs@clembs.com>
2024-10-20 00:35:39 +0200

    feat: about me widget component

---
+++ src/lib/widgets/default/AboutMeWidgetComponent.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Card from '$lib/components/Card.svelte';
+	import type { User } from '$lib/db/schema/users';
+	import type { AboutMeWidget } from '../types';
+
+	let {
+		widget
+	}: {
+		user: Partial<User>;
+		widget: AboutMeWidget;
+	} = $props();
+</script>
+
+<Card>
+	<div class="about-me">
+		<h2>About me</h2>
+		<p>{widget.content}</p>
+	</div>
+</Card>
+
+<style>
+	.about-me {
+		display: flex;
+		flex-direction: column;
+		gap: var(--base-gap);
+	}
+	h2 {
+		font-size: 1.25rem;
+	}
+</style>
